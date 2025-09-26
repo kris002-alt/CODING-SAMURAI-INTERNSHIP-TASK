d70cfd8330fd90ec5334e1d0c34b4b7cb6aece05
krishna-bhattarai <k159rishna@gmail.com>
2025-09-26 17:53:32 +0545

    Initial project commit

---
+++ AtmSystem/src/test/java/com/example/AtmSystem/AtmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AtmSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
