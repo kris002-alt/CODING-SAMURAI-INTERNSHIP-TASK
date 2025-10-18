164fa5b48568fe54d95d9c7268c8fd788dd00570
krishna-bhattarai <k159rishna@gmail.com>
2025-10-18 21:42:17 +0545

    initial commit

---
+++ AtmSystem/src/main/java/com/example/AtmSystem/models/Transactions.java
@@ -0,0 +1,87 @@
+package com.example.AtmSystem.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "transactions")
+public class Transactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    private LocalDateTime timestamp;
+
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    // Constructors, getters, setters
+
+    public Transactions(Long id, BigDecimal amount, TransactionType transactionType, LocalDateTime timestamp, String description, Account account) {
+        this.id = id;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.timestamp = timestamp;
+        this.description = description;
+        this.account = account;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
